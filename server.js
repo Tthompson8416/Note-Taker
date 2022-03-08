e699a4648205aa03c702454ca5723065b797aa3e
TT84 <tthompson8416@outlook.com>
2022-03-08 17:41:28 -0500

    createNewNote function added

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const htmlRoutes = require('./routes/htmlRoutes')
 const app = express()
 const PORT = 3009
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+function createNewNote(body, createNewNote {
+    console.log(body);
+    // our function's main code will go here!
+  
+    // return finished code to post route for response
+    return body;
+  }
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static('public'))
 app.use('/api', apiRoutes)
 app.use('/', htmlRoutes)
